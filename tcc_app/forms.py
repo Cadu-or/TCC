e22eba8df2d3c31148d0015271bcc5f366e1e23a
Carlos Eduardo <carlos.e.o.ribeiro@gmail.com>
2023-06-13 17:53:56 -0300

    🚧 Pagina inicial sendo construída.
    Co-authored-by: João Gabriel Saraiva <Joaofsrs@users.noreply.github.com>

---
+++ tcc_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+import pandas as pd
+
+dataset = pd.read_csv('tcc_app/static/tcc_app/csv/serie_2000a2021.csv')
+choices = tuple()
+
+for i in pd.unique(dataset['CODE']):
+  auxiliar = ((i,i),)
+  choices = choices + auxiliar
+
+class IndicadoresForm(forms.Form):
+  Indicador1 = forms.ChoiceField(choices=choices, label="")
+  Indicador2 = forms.ChoiceField(choices=choices, label="")
