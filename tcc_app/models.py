6497a457b9e2ecf8e8088cf127ef9fe4dcee7199
Carlos Eduardo <carlos.e.o.ribeiro@gmail.com>
2023-06-13 17:49:35 -0300

    🚧 Configurando o banco de dados.
    Co-authored-by: João Gabriel Saraiva <Joaofsrs@users.noreply.github.com>

--- tcc_app/models.py
+++ tcc_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+import pandas as pd
 # Create your models here.
+class Correlacao(models.Model):
+  code1 = models.CharField(max_length=255)
+  code2 = models.CharField(max_length=255)
+  correlacao = models.FloatField()
+  delay = models.IntegerField()
